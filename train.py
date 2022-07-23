791855d58c02bf7d561a7add9244a6d4fc897541
li-chuan-1998 <chuanster1998@gmail.com>
2022-07-23 15:08:49 +0900

    main

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
+import numpy as np
 import argparse
 
 from models.pcn import PCN
-from loss_utils.emd import *
-from loss_utils.cd import *
+from loss_utils.utils import *
 from dataloader import Dataloader
 
 def train(args):
@@ -26,8 +26,8 @@ def train(args):
         print("\nStart of epoch %d" % (epoch+1,))
 
         for step, (inputs, npts, gt) in enumerate(ds_train_iter):
-            inputs = tf.convert_to_tensor(inputs,np.float32)
-            gt = tf.convert_to_tensor(gt,np.float32)
+            inputs = tf.convert_to_tensor(inputs, np.float32)
+            gt = tf.convert_to_tensor(gt, np.float32)
             print(type(inputs),type(npts),type(gt))
             print(inputs.shape,sum(npts),gt.shape)
             with tf.GradientTape() as tape:
@@ -35,17 +35,11 @@ def train(args):
                 coarse, fine = model((inputs, npts), training=True)  # Logits for this minibatch
                 print(coarse.shape,fine.shape)
 
-                # Compute the loss value for this minibatch.
-                loss_value = sum(model.losses)
-
                 """Total Loss Calculation"""
-                # gt_ds = gt[:, :coarse.shape[1], :]
-                # loss_coarse = getEMD(coarse.numpy(), gt_ds.numpy())
-
-                loss_coarse = chamfer_distance_tf(coarse, gt[:, :coarse.shape[1], :])/2
-                loss_fine = chamfer_distance_tf(fine, gt)/2
-                total_loss = loss_coarse + loss_fine
-                loss_value += total_loss 
+                gt_ds = gt[:, :coarse.shape[1], :]
+                loss_coarse = earth_mover(coarse, gt_ds)
+                loss_fine = chamfer(fine, gt)
+                loss_value = loss_coarse + loss_fine
 
             print(len(model.trainable_weights),type(loss_value))
             grads = tape.gradient(loss_value, model.trainable_weights)
@@ -65,7 +59,7 @@ if __name__ == '__main__':
     parser.add_argument('--log_dir', default="/content/drive/MyDrive/pcn_altered_8192")
     parser.add_argument('--restore', action='store_true')
     parser.add_argument('--num_epochs', type=int, default=1000)
-    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--num_gt_points', type=int, default=16384)
     parser.add_argument('--base_lr', type=float, default=0.0001)
     parser.add_argument('--decay_steps', type=int, default=30000)
