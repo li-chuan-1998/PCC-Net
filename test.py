2ac069281bc444ba50666f9aae4a608b4cc8b434
li-chuan-1998 <chuanster1998@gmail.com>
2022-08-01 11:34:09 +0900

    main

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from datagenerator import DataGenerator
 from models.pcn import PCN
 
 def train(args):
-    ds_test = DataGenerator(os.listdir(args.test_dir), complete_dir=args.data_path, batch_size=args.batch_size)
+    ds_test = DataGenerator(os.listdir(args.test_dir), complete_dir=args.test_dir, batch_size=args.batch_size)
 
     # Model Initialization
     model = PCN()
@@ -17,10 +17,11 @@ def train(args):
     model.load_weights(latest)
 
     # Testing
+    base = 0.01
     for id, (input, npts, gt) in enumerate(ds_test):
         coarse, fine = model((input, npts), training=True)
         if args.visualise_outputs:
-            show_pcds([input[0], fine[0], gt[0]])
+            show_pcds([input[0], fine[0] + base, gt[0]+base*2])
 
         if args.save_outputs:
             #TODO
@@ -32,6 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--checkpoint_dir', default="/content/drive/MyDrive/pcn_tf_2/")
     parser.add_argument('--visualise_outputs', action='store_true')
     parser.add_argument('--save_outputs', action='store_true')
+    parser.add_argument('--batch_size', type=int, default=1)
 
     args = parser.parse_args()
 
